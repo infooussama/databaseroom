cce4acb663a34045fff6b72d24888f540c191f81
DESKTOP-F957URJ\Hamada <info.h.oussama@gmail.com>
2020-11-04 22:43:48 +0100

    4/11/2020

--- app/src/main/java/com/example/relationbdd/acs/Ant.java
+++ app/src/main/java/com/example/relationbdd/acs/Ant.java
@@ -195,9 +195,38 @@ public class Ant {
    public static int DISTANCE_MAX;	//set in constructor so it is relative to graph size
     private static final double PHEROMONE_MAX = 10.0;
     private FullStation currentPos;
-    private final int id = 0;
+    private int id = 0;
     private static int numAnts = 0;
-    private ArrayList<LigneDB> path;
-    FullStation end;
+    private ArrayList<LigneDB> traveledTo,path;
+    FullStation start,end;
+    public Ant(Graphf graph, FullStation start,FullStation end){
+        //if(terminus.contains(start)&&terminus.contains(end))
+        DISTANCE_MAX = graph.graph().size();//nbr de station dans le graph(terminus 500-->200)
+        //else if(terminus.contains(start))
+        DISTANCE_MAX = graph.graph().size()+1;
+        //else if(terminus.contains(end))
+        DISTANCE_MAX = graph.graph().size()+1;
+        //else
+        DISTANCE_MAX = graph.graph().size()+2;
 
+        this.start = start;
+        this.end = end;
+        id = numAnts;
+        numAnts++;
+        currentPos = start;
+        traveledTo = new ArrayList<>();
+        path = new ArrayList<>();
+    }
+
+    public int walk(){
+        int status = move();
+        while(status == 0){
+            status = move();
+        }
+        return status;
+    }
+
+    public int move(){
+        return 0;
+    }
 }
--- app/src/main/java/com/example/relationbdd/acs/Graphf.java
+++ app/src/main/java/com/example/relationbdd/acs/Graphf.java
@@ -14,5 +14,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Graphf extends AppCompatActivity {
 
+    List<FullStation> stations;
+    List<FullStation> chosenStation,unused;
+    private int nbrNodes;
+    private int nbrRoute;
+    private int min;
+    private int max;
+    int ligneLenght,lenght=0;
+    FullStationDao fullStationDao;
+    LigneDao ligneDao;
+    RoomDB roomDB;
+    ArrayList<FullStation> nodes;
+    ArrayList<ArrayList<FullStation>> routes;
+    Random r = new Random();
+    List<LigneDB> ligneDBS;
+    FullStation item,item2;
+    int index,reverse;
 
+    public ArrayList<ArrayList<FullStation>> graph(){
+        stations = fullStationDao.getAllStations();
+        chosenStation = new ArrayList<>();
+        routes = new ArrayList<ArrayList<FullStation>>(nbrRoute);
+        for(int count = 1; count<=nbrRoute ;count++){
+            nodes = new ArrayList<FullStation>();
+            ligneLenght = r.nextInt(min-max) + min;
+            if(count == 1){
+                index = r.nextInt(stations.size());
+                item = stations.get(index);
+                nodes.add(item);
+                routes.add(nodes);
+            }else {
+                index = r.nextInt(stations.size());
+                item = stations.get(index);
+                chosenStation.add(item);
+                routes.get(count).add(item);
+            }
+            lenght++;
+            reverse = 1;
+            while (lenght<ligneLenght && reverse > 0){
+                ligneDBS = ligneDao.getFullStationLignes(item.getScode());
+                /*function treje3 stationsArrivres fetouhi yahsel biha return list1*/
+                /* inused = remove from list1 (les station li rahom f routes.get(count))*/
+                if(unused != null){
+                    lenght++;
+                    index = r.nextInt(unused.size());
+                    item2 = unused.get(index);
+                    routes.get(count).add(item2);
+                    item = item2;
+                    chosenStation.add(item2);
+                }else {
+                    reverse--;
+                }
+            }
+        }
+        return routes;
+    }
 }
